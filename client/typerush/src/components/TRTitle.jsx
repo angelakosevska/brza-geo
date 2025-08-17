01bb389fa9b2fd12dff5dc0788aa2ff917bf92a6
Angela <angelakjosevska@gmail.com>
2025-08-17 20:52:04 +0200

    client

--- client/typerush/src/components/TRTitle.jsx
+++ client/typerush/src/components/TRTitle.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import TRTitle2 from "./TRtitle2"; // Adjust path if needed
+//import TRTitle2 from "./TRtitle2"; // Adjust path if needed
 
 export default function TRTitle() {
   return (
     <>
       {/* Animated TRTitle2 on desktop and larger */}
-      <div className="hidden md:flex">
-        <TRTitle2 />
-      </div>
+      <div className="hidden md:flex">{/* <TRTitle2 /> */}</div>
 
       {/* Simple stacked version for mobile and tablets */}
       <div className="flex flex-col md:hidden items-center gap-1">
