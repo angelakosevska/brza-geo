db0a43b84b6024b8b0b4f236f13080c2bf44cd6c
Angela <you@example.com>
2025-09-26 21:40:11 +0200

    Mail

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,21 +1,34 @@
-const transporter = require("./mailer");
+const sgMail = require("@sendgrid/mail");
+
+// земаме API KEY од .env
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.sendWelcomeEmail = async (user) => {
-  await transporter.sendMail({
-    from: `"Type Rush" <brza.geografija17@gmail.com>`,
+  const msg = {
     to: user.email,
+    from: {
+      email: process.env.SENDGRID_FROM, // ова мора да биде верифициран sender во SendGrid
+      name: "Type Rush",
+    },
     subject: "Добредојде во Type Rush!",
     html: `<p>Здраво ${user.username},<br/>Добре дојде во Type Rush! Повикај ги пријателите и забавувајте се!</p>`,
-  });
+  };
+
+  await sgMail.send(msg);
 };
 
 exports.sendPasswordResetEmail = async (user, code) => {
-  await transporter.sendMail({
-    from: `"Type Rush" <brza.geografija17@gmail.com>`,
+  const msg = {
     to: user.email,
-    subject: "Твојот код за промена на лозинка на Type Rush.",
+    from: {
+      email: process.env.SENDGRID_FROM,
+      name: "Type Rush",
+    },
+    subject: "Твојот код за промена на лозинка на Type Rush",
     html: `<p>Здраво ${user.username},</p>
            <p>Твојот код за промена на лозинката е: <strong>${code}</strong></p>
            <p>Внимавај, истекува за 15 минути.</p>`,
-  });
+  };
+
+  await sgMail.send(msg);
 };
