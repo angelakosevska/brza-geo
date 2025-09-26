1c2309e9052b012c5fa532116788f9a95a426ba9
Angela <you@example.com>
2025-09-26 19:11:07 +0200

    Mail fix

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
